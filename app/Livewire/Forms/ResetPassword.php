6cc94fd87f9a21089eff57fe9af840fafc28ede0
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2023-11-16 15:39:33 +0100

    Prevent users from double submitting password reset by giving them a confirmation

--- app/Livewire/Forms/ResetPassword.php
+++ app/Livewire/Forms/ResetPassword.php
@@ -10,12 +10,16 @@ class ResetPassword extends Component
 {
     public ResetPasswordForm $form;
 
+    public $submitted = false;
+
     public function resetPassword()
     {
         $this->validate();
 
         $user = User::where('email', $this->form->email)->first();
         if($user) $user->notify(new NotificationsResetPassword($user, $user->createToken('reset-password')->plainTextToken));
+
+        $this->submitted = true;
     }
 
     public function render()
--- resources/views/livewire/forms/reset-password.blade.php
+++ resources/views/livewire/forms/reset-password.blade.php
@@ -1,5 +1,9 @@
 <form wire:submit="resetPassword" class="space-y-4">
-    <x-input.text name="form.email" label="Email" />
+    @if(!$submitted)
+        <x-input.text name="form.email" label="Email" />
 
-    <x-input.button type="submit">Wachtwoord resetten</x-input.button>
+        <x-input.button type="submit">Wachtwoord resetten</x-input.button>
+    @else
+        <div class="text-green-600 bg-green-100 p-4 rounded-sm">Indien het opgegeven e-mailadres bij ons bekend is, ontvangt u een e-mail met een link om uw wachtwoord opnieuw in te stellen.</div>
+    @endif
 </form>
