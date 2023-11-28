1b6ccf7802635cfca36685f86fc2dc4dc850f71c
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2023-11-28 08:57:11 +0100

    Added data to the users nutrition plans table

--- resources/views/livewire/user-nutrition-plans-table.blade.php
+++ resources/views/livewire/user-nutrition-plans-table.blade.php
@@ -3,6 +3,13 @@
         <table class="table-fixed">
             <thead>
                 <th>Datum</th>
+                <th>Ma</th>
+                <th>Di</th>
+                <th>Wo</th>
+                <th>Do</th>
+                <th>Vr</th>
+                <th>Za</th>
+                <th>Zo</th>
                 <th></th>
             </thead>
 
@@ -10,6 +17,13 @@
                 @foreach($nutritionPlans as $nutritionPlan)
                     <tr>
                         <td>{{ $nutritionPlan->date->format('d-m-Y') }}</td>
+                        <td>@if(empty($nutritionPlan->recipies_monday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_tuesday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_wednesday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_thursday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_friday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_saturday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
+                        <td>@if(empty($nutritionPlan->recipies_sunday ))<span class="text-red-500"><i class="fa-solid fa-xmark"></i></span>@else<span class="text-primary"><i class="fa-solid fa-check"></i></span>@endif</td>
                         <td>
                             <button wire:click="$dispatch('openModal', {component: 'forms.nutrition-plan', arguments: {id: {{ $nutritionPlan->id }}}})"
                             class="text-xs bg-primary text-white p-2 rounded-md hover:bg-green-600"><i class="fas fa-edit"></i></button>
