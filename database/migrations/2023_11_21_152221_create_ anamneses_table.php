fb4bff8dd576f74c4d8a98f0f55264809dcc7d48
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2023-11-30 10:26:39 +0100

    Changed the anamneses table to plural name

--- database/migrations/2023_11_21_152221_create_ anamneses_table.php
+++ database/migrations/2023_11_21_152221_create_ anamneses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('anamnesis', function (Blueprint $table) {
+        Schema::create('anamneses', function (Blueprint $table) {
             $table->id();
 
             $table->string('gastrointestinal_issues')->nullable();                   // Maag- of darmklachten
