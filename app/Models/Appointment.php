2951e64be60b4082107547baa51e3a3b1eff4af1
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2023-12-14 00:06:14 +0100

    Added appointments table and Appointment model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'branch_id',
+        'service_id',
+        'user_id',
+        'start',
+        'end',
+    ];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeBetween($query, $start, $end)
+    {
+        return $query
+            ->where('start', '>=', $start)
+            ->where('end', '<=', $end);
+    }
+}
