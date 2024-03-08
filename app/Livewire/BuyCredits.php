9183923b750e2b199b575eb11501b6c6b0732cda
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-03-08 16:19:01 +0100

    Mollie webhook URL

--- app/Livewire/BuyCredits.php
+++ app/Livewire/BuyCredits.php
@@ -39,7 +39,7 @@ class BuyCredits extends ModalComponent
             ],
             'description' => $creditOption->credits . ' credits. ' . $creditOption->validityPeriodString(),
             'redirectUrl' => route('dashboard'),
-            'webhookUrl'  => 'https://57a47963b74e.ngrok.app/webhooks/mollie', //route('webhooks.mollie'),
+            'webhookUrl'  => route('webhooks.mollie'),
             'metadata'    => [
                 'credit_order_id' => $creditOrder->id,
             ],
