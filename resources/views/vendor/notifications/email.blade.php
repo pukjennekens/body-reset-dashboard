28fa3b0af21809467f344b281922c90992f8353f
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-03-19 16:16:01 +0100

    Typo fix

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -47,7 +47,7 @@
 @isset($actionText)
 <x-slot:subcopy>
 @lang(
-    "Als u niet op de \":actionText\" knop kunt drukken, kopieer en plak dan onderstaande URL in uw browser:\n",
+    "Als je niet op de \":actionText\" knop kunt drukken, kopieer en plak dan onderstaande URL in je browser:\n",
     [
         'actionText' => $actionText,
     ]
