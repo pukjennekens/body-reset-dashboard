525cf45e7c9757ec2d1a3bab91a706224622891d
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-03-20 10:13:06 +0100

    Select bg color

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,6 +1,6 @@
 <div>
     <label for="{{ $attributes->get('name') }}" class="block mb-1">{{ $attributes->get('label') }}</label>
-    <select class="rounded-lg px-4 py-1.5 w-full border border-gray-600 disabled:bg-gray-200" wire:model.fill="{{ $attributes->get('name') }}" @if($attributes->get('disabled')) disabled @endif>
+    <select class="rounded-lg px-4 py-1.5 w-full border border-gray-600 disabled:bg-gray-200 bg-white" wire:model.fill="{{ $attributes->get('name') }}" @if($attributes->get('disabled')) disabled @endif>
         {{ $slot }}
     </select>
     @error($attributes->get('name')) <span class="text-red-500 text-sm">{{ $message }}</span> @enderror 
