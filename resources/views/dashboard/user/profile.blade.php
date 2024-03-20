a5980ea00d38db8cf76c3023698f933b773f2c46
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-03-20 11:35:42 +0100

    Trello fixed

--- resources/views/dashboard/user/profile.blade.php
+++ resources/views/dashboard/user/profile.blade.php
@@ -10,9 +10,11 @@
             @livewire('forms.change-password')
         </div>
 
-        <button type="button" class="text-xs bg-red-500 text-white p-2 rounded-md hover:bg-red-700 block mt-4" x-data x-on:click.prevent="$dispatch('openModal', {component: 'delete-user', arguments: {id: {{ auth()->id() }} }})">
-            <i class="fa-solid fa-trash"></i>
-            Je account verwijderen
-        </button>
+        @if(auth()->user()->hasRole('user'))
+            <button type="button" class="text-xs bg-red-500 text-white p-2 rounded-md hover:bg-red-700 block mt-4" x-data x-on:click.prevent="$dispatch('openModal', {component: 'delete-user', arguments: {id: {{ auth()->id() }} }})">
+                <i class="fa-solid fa-trash"></i>
+                Je account verwijderen
+            </button>
+        @endif
     </div>
 </x-dashboard-layout>
